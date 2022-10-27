9f807994d2463728fc248f0feb21af2aa2ee5f2b
aidata <aidata@10.121.3.191>
2022-10-27 10:13:12 +0300

    array2

--- deneme/src/deneme/gfhg.java
+++ deneme/src/deneme/gfhg.java
@@ -3,7 +3,9 @@ package deneme;
 public class gfhg {
 
 	public static void main(String[] args) {
-		int array[]= new int[10];
+		
+		int array[]= {32,27,64,18,95,14,90,70,60,17};
+		
 		System.out.printf("%s%8s%n","Intex","Value");
 		for (int counter=0;counter<array.length;counter++) {
 			System.out.printf("%5d%8d%n",counter,array[counter]);;
