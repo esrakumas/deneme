88efa8e4823c2dd501bf60bb1bc5ab84c426b4fa
aidata <aidata@10.121.3.191>
2022-10-27 10:10:31 +0300

    mesaj3

--- deneme/src/deneme/gfhg.java
+++ deneme/src/deneme/gfhg.java
@@ -3,7 +3,12 @@ package deneme;
 public class gfhg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[]= new int[10];
+		System.out.printf("%s%8s%n","Intex","Value");
+		for (int counter=0;counter<array.length;counter++) {
+			System.out.printf("%5d%8d%n",counter,array[counter]);;
+			
+		}
 
 	}
 
