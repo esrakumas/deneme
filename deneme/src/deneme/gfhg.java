d54cd8601a618287dd5c3bf287f5e11826acc4ea
aidata <aidata@10.121.3.191>
2022-10-27 10:38:03 +0300

    ters cevirme

--- deneme/src/deneme/gfhg.java
+++ deneme/src/deneme/gfhg.java
@@ -6,11 +6,27 @@ public class gfhg {
 		
 		int array[]= {32,27,64,18,95,14,90,70,60,17};
 		
-		System.out.printf("%s%8s%n","Intex","Value");
-		for (int counter=0;counter<array.length;counter++) {
-			System.out.printf("%5d%8d%n",counter,array[counter]);;
+
+		int i=9;
+
+		for (int counter=0;counter<array.length/2;counter++) {
+			int temp;
 			
-		}
+			array[counter]=array[i];
+			temp=array[i];
+			i--;
+	}
+		
+		for (int counter=0;counter<array.length;counter++) {
+			
+			System.out.printf("%5d%8d%n",counter,array[counter]);
+	
+	}
+	
+		
+			
+			
+		
 
 	}
 
